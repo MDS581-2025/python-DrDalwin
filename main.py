334a05415265ba3636002abbae25ff0604ed08e4
DrDalwin <112147763+DrDalwin@users.noreply.github.com>
2025-11-07 12:55:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 # TODO: Write your code here
 # Example input: 5
 # Example output: Positive number
+# main.py
+def check_number(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "Zero"
